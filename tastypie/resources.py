79177278e489911575d7907225863e8fb702a2b2
Glen Coates <glen@handshake-app.com>
2012-10-23 17:16:27 +1100

    Process deletions before updates in patch_list()

--- tastypie/resources.py
+++ tastypie/resources.py
@@ -1387,6 +1387,16 @@ class Resource(object):
         if collection_name not in deserialized:
             raise BadRequest("Invalid data sent: missing '%s'" % collection_name)
 
+        # Do deletions before creations so we don't accidentally blow away things that were just created
+        deleted_collection = deserialized.get(deleted_collection_name, [])
+        if deleted_collection:
+            if 'delete' not in self._meta.detail_allowed_methods:
+                raise ImmediateHttpResponse(response=http.HttpMethodNotAllowed())
+
+            for uri in deleted_collection:
+                obj = self.get_via_uri(uri, request=request)
+                self.obj_delete(request=request, _obj=obj)
+
         if len(deserialized[collection_name]) and 'put' not in self._meta.detail_allowed_methods:
             raise ImmediateHttpResponse(response=http.HttpMethodNotAllowed())
 
@@ -1421,15 +1431,6 @@ class Resource(object):
                 self.obj_create(bundle, request=request)
 
             bundles_seen.append(bundle)
-
-        deleted_collection = deserialized.get(deleted_collection_name, [])
-        if deleted_collection:
-            if 'delete' not in self._meta.detail_allowed_methods:
-                raise ImmediateHttpResponse(response=http.HttpMethodNotAllowed())
-
-            for uri in deleted_collection:
-                obj = self.get_via_uri(uri, request=request)
-                self.obj_delete(request=request, _obj=obj)
 
         if not self._meta.always_return_data:
             return http.HttpAccepted()
